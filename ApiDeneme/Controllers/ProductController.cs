56cd5913ce3b4711751fc6beca58c3d7adccaae3
Servet Canberk Yangaz <canberk.yangaz@tat.com.tr>
2024-09-04 08:32:31 +0300

    ProductController

---
+++ ApiDeneme/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiDeneme.Controllers
+{
+    // localhost:5000/api/products port örnek amaçlı
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+
+        public static readonly string[] Products = {
+            "Iphone 11","Iphone 12","Iphone 13","Iphone 14","Iphone 15","Iphone 16"
+        };
+
+        // localhost:5000/api/products => GET
+        [HttpGet]
+        public string[] GetProducts()
+        {
+            return Products;
+        }
+
+        // localhost:5000/api/products/1 => GET
+        [HttpGet("{id}")]
+        public string GetProduct(int id)
+        {
+            return Products[id];
+        }
+
+    }
+}
