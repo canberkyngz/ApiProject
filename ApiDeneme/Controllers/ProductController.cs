613bb0ff06df09a914550ae21c05112216259eeb
Servet Canberk Yangaz <canberk.yangaz@tat.com.tr>
2024-09-04 09:37:53 +0300

    Product Model

--- ApiDeneme/Controllers/ProductController.cs
+++ ApiDeneme/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ApiDeneme.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ApiDeneme.Controllers
 {
@@ -8,23 +9,49 @@ namespace ApiDeneme.Controllers
     public class ProductController : ControllerBase
     {
 
-        public static readonly string[] Products = {
-            "Iphone 11","Iphone 12","Iphone 13","Iphone 14","Iphone 15","Iphone 16"
-        };
+        public static List<Product>? _products;
+
+        public ProductController()
+        {
+            _products = new List<Product>
+            {
+               new Product { ProductId=1, ProductName = "Iphone 11", Price = 60000, IsActive = true},
+               new Product { ProductId=2, ProductName = "Iphone 12", Price = 70000, IsActive = true},
+               new Product { ProductId=3, ProductName = "Iphone 13", Price = 80000, IsActive = true},
+               new Product { ProductId=4, ProductName = "Iphone 14", Price = 90000, IsActive = true},
+               new Product { ProductId=5, ProductName = "Iphone 15", Price = 100000, IsActive = true}
+            };
+
+        }
 
         // localhost:5000/api/products => GET
         [HttpGet]
-        public string[] GetProducts()
+        public IActionResult GetProducts()
         {
-            return Products;
+            if (_products == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(_products);
         }
 
         // localhost:5000/api/products/1 => GET
         [HttpGet("{id}")]
-        public string GetProduct(int id)
+        public IActionResult GetProduct(int? id)
         {
-            return Products[id];
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var p =_products?.FirstOrDefault(i=>i.ProductId == id);
+            if (p == null)
+            {
+                return NotFound();
+            }
+            return Ok(p);
         }
 
     }
-}
+}   
